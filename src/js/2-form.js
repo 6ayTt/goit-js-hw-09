c31c5f2c99a17b5e1e554d9c310be12066e82129
Vitalii <vitalik222014@gmail.com>
2026-09-17 23:22:06 +0300

    1

---
+++ src/js/2-form.js
@@ -0,0 +1,45 @@
+const GET_KEY = "feedback-form-state";
+const getForm = document.querySelector(".feedback-form");
+const emailInput = getForm.querySelector('input[name="email"]');
+const messageInput = getForm.querySelector('textarea[name="message"]');
+getForm.addEventListener("input", changeValueField);
+getForm.addEventListener("submit", submitForm);
+
+const formData = {
+    email: "",
+    message: ""
+};
+
+function changeValueField (event) {
+    const getNameField = event.target.name;
+    formData[getNameField] = event.target.value;
+    const createEntryJson = JSON.stringify(formData);
+    localStorage.setItem(GET_KEY, createEntryJson);
+}
+
+if(!localStorage.getItem(GET_KEY)){
+
+    } else {
+    const geItemWithLocalStorage = JSON.parse(localStorage.getItem(GET_KEY));
+    formData.email = geItemWithLocalStorage.email;
+    formData.message = geItemWithLocalStorage.message;
+
+    emailInput.value = geItemWithLocalStorage.email;
+    messageInput.value = geItemWithLocalStorage.message;
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    if(emailInput.value.length === 0 || messageInput.value.length === 0) {
+        alert("Fill please all fields");
+    } else {
+        console.log(formData);
+        localStorage.clear();
+        formData.email = "";
+        formData.message = "";
+        emailInput.value = "";
+        messageInput.value = "";
+    }
+    
+    
+}
